6c6536569483d5c4ad7a038626f258abb3fd02f3
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2023-04-30 18:43:44 -0500

    Suma la cantidad de tipos de café

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -10,15 +10,32 @@ import { Cafe } from './cafe';
 export class CafeComponent implements OnInit {
 
   variedadesCafe: Array<Cafe> =[];
+  cantidadOriginal: number=0;
+  cantidadBlend: number=0;
+
   constructor( private cafeService: CafeService) { }
 
   getVariedadesCafe(): void {
     this.cafeService.getVariedadesCafe().subscribe((variedadesCafe) => {
       this.variedadesCafe = variedadesCafe;
+      this.cantidadesPorVariedad();
     });
   }
 
   ngOnInit() {
     this.getVariedadesCafe();
+  }
+
+  cantidadesPorVariedad(){
+    console.log('tipo lenght--> '+this.variedadesCafe.length);
+
+    for (let i = 0; i < this.variedadesCafe.length; i++) {
+      console.log('tipo--> '+this.variedadesCafe[i].tipo);
+      if(this.variedadesCafe[i].tipo == 'Café de Origen'){
+        this.cantidadOriginal = this.cantidadOriginal + 1;
+      }else if(this.variedadesCafe[i].tipo == 'Blend'){
+        this.cantidadBlend = this.cantidadBlend + 1;
+      }
+    }
   }
 }
